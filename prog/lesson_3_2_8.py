9c468b237f7fc23aa977008cc1cc17c6fd267627
Your Name <51780751+michail82@users.noreply.github.com>
2023-01-08 20:20:12 +0300

    add lesson_3_2_8

--- prog/lesson_3_2_8.py
+++ prog/lesson_3_2_8.py
@@ -1,5 +1,5 @@
 def test_input_text(expected_result, actual_result):
-    
+    assert self.is_element_present('create_class_button', timeout=30), "No create class button" 
     pass
     # ваша реализация, напишите assert и сообщение об ошибке
     
