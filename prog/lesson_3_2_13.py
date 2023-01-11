a75d907919c1fd3898778152fc065f16902aa513
Your Name <51780751+michail82@users.noreply.github.com>
2023-01-11 17:50:39 +0300

    update

---
+++ prog/lesson_3_2_13.py
@@ -0,0 +1,78 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.XPATH, '//input[contains(@placeholder,"Input your first name")]'); 
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.XPATH, '//input[contains(@placeholder,"Input your last name")]')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.XPATH, '//input[contains(@placeholder,"Input your email")]')
+        input3.send_keys("smolensk@dddd.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+               
+               
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element(By.XPATH, '//input[contains(@placeholder,"Input your first name")]'); 
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.XPATH, '//input[contains(@placeholder,"Input your last name")]')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.XPATH, '//input[contains(@placeholder,"Input your email")]')
+        input3.send_keys("smolensk@dddd.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ prog/lesson_3_2_9.py
@@ -0,0 +1,8 @@
+def test_substring(full_string, substring):
+    assert substring in full_string,\
+        f" expected '{substring}' to be substring of '{full_string}'"
+
+full_string = 'some_text'
+substring = 'some'
+
+test_substring(full_string, substring)
